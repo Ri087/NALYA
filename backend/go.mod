f254844e9d15f99616ee054e63d25c09a7544992
Ri087 <jeremydura@hotmail.fr>
2022-06-28 18:25:48 +0200

    initialisation bdd

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module nalya
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.13
+)
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"nalya/nalya"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // type App struct {
 // 	Router *mux.Router
 // 	DB     *sql.DB
 // }
+func initDataBase(database string) *sql.DB {
+	db, err := sql.Open("sqlite3", database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	SqltStmt := `
+	CREATE TABLE IF NOT EXISTS users (
+					id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+					name TEXT NOT NULL,
+					email TEXT NOT NULL,
+					password TEXT NOT NULL
+				);
+				`
+	_, err = db.Exec(SqltStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func insertIntoUsers(db *sql.DB, name string, email string, password string) (int64, error) {
+	result, _ := db.Exec(`INSERT INTO users (name,email,password) VALUES (?,?,?)`, name, email, password)
+	return result.LastInsertId()
+
+}
 
 func main() {
+
+	db := initDataBase("NALYA_DB.db")
+	fmt.Println(db)
+
+	insertIntoUsers(db, "admin", "admin@admin.admin", "azeaze")
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/getData", nalya.DataHandler)
 	http.ListenAndServe(":8080", nil)
